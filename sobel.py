b9c82739203f21af312adaa1bc52497968b5a5c0
spra0027 <89630628+spra0027@users.noreply.github.com>
2023-05-10 16:25:17 +1000

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -29,28 +29,4 @@ image = cv2.imread('Screenshot 2023-05-10 at 3.57.41 pm.png')
 
 gradient_magnitude, gradient_direction = apply_sobel_filter(image)
 
-# Normalize the gradient magnitude for visualization
-gradient_magnitude = cv2.normalize(gradient_magnitude, None, 0, 255, cv2.NORM_MINMAX)
-
-# Convert gradient direction to degrees for visualization
-gradient_direction_degrees = gradient_direction * 180 / np.pi
-gradient_direction_degrees += 180
-
-# Convert to uint8 for visualization
-gradient_direction_degrees = gradient_direction_degrees.astype(np.uint8)
-
-# Create HSV image for visualization
-hsv = np.zeros_like(image)
-hsv[..., 0] = gradient_direction_degrees
-hsv[..., 1] = 255
-hsv[..., 2] = gradient_magnitude
-
-# Convert back to BGR for display
-bgr = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-
-
 print(gradient_magnitude)
-# Display the output image
-cv2.imshow('Gradient', bgr)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
