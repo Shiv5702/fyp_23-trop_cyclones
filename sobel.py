3ba63d2595bfac383e4a5f631c3f01225c2e3998
spra0027 <89630628+spra0027@users.noreply.github.com>
2023-05-10 15:44:11 +1000

    Update sobel.py to add smoothing filter

--- sobel.py
+++ sobel.py
@@ -11,13 +11,31 @@ from PIL import Image
 from scipy.signal import convolve2d
 import cv2
 
+
+ ##The code applies the 
+ # Sobel filter to calculate the gradient in the x and y directions separately, 
+ # and then combines them to calculate the total gradient at each pixel. The resulting 
+ # output is a numpy array with two matrices: gradient magnitude and gradient direction. 
+ # The magnitude represents the strength of the gradient, and the direction represents the 
+ # direction of the gradient at each pixel. The output can be visualized as arrows pointing 
+ # towards the center of the storm, where larger arrows represent a higher gradient magnitude.
+
+import numpy as np
+from PIL import Image
+from scipy.signal import convolve2d
+import cv2
+
+
 def apply_sobel_filter(image):
     sobel_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
 
     # Convert to grayscale if needed
     if image.ndim == 3:
-        image = np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply smoothing filter
+    image = cv2.GaussianBlur(image, (3, 3), 0)
 
     # Apply Sobel filters
     gradient_x = convolve2d(image, sobel_x, mode='same', boundary='symm') / 8.0
@@ -28,8 +46,7 @@ def apply_sobel_filter(image):
     gradient_direction = np.arctan2(gradient_y, gradient_x)
 
     return gradient_magnitude, gradient_direction
-
-
+   
 # Load the image and convert it to a numpy array
 image = cv2.imread('my_plot2x.jpg')
 print(apply_sobel_filter(image))
