0a021dfb76a5ddcfeba4f6ab2a7d1658c192d6a2
Shakthi <spra0027@student.monash.edu>
2023-08-23 11:56:58 +1000

    tracking the clusters on the plot

--- tracking2.py
+++ tracking2.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
 # Load the pre-calculated DAV array
-dav_array = np.load("dav_values.npy")
+dav_array = np.load("fyp_23-trop_cyclones/dav_values.npy")
 
 # Set threshold values
 dav_threshold = 3100  # Adjust this threshold as needed
@@ -60,7 +60,14 @@ def overlay_clusters_on_dav_map(dav_values, clusters):
     # Plot the clusters as points or markers
     for cluster in clusters:
         x_coords, y_coords = zip(*cluster)
-        plt.scatter(y_coords, x_coords, color='red', s=10)  # You can adjust the color and size as needed
+        cluster_center_x = np.mean(x_coords)
+        cluster_center_y = np.mean(y_coords)
+        cluster_radius = max(np.ptp(x_coords), np.ptp(y_coords)) / 2  # Compute the radius as half of the maximum extent
+        circle = plt.Circle((cluster_center_y, cluster_center_x), cluster_radius, color='red', fill=False, lw=1)
+        plt.gca().add_patch(circle)
+        #x_coords, y_coords = zip(*cluster)
+        #plt.scatter(y_coords, x_coords, color='red', s=10)  # You can adjust the color and size as needed
+
 
     plt.colorbar()
     plt.title('Tracked Clusters on DAV Map')
