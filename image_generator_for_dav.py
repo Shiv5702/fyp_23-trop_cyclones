dcec90bf88564de21540ee044aa6032ea27db391
Jason Tay <jtay0034@student.monash.edu>
2023-08-30 11:30:18 +1000

    regenerated images

--- image_generator_for_dav.py
+++ image_generator_for_dav.py
@@ -12,8 +12,8 @@ nc = netCDF4.Dataset('resampled_file.nc4')
 var = nc.variables['Tb']
 
 # Get the latitude and longitude values
-lat = nc.variables['lat'][::2]
-lon = nc.variables['lon'][::2]
+lat = nc.variables['lat'][:]
+lon = nc.variables['lon'][:]
 
 # Define the North Atlantic region (in degrees)
 lon_min, lon_max = -120, 0
