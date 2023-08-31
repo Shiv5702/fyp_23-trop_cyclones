cde1cb53d5bdec1a3df008fc97661d8dfb17a24a
Shakthi <spra0027@student.monash.edu>
2023-08-31 13:29:41 +1000

    changed the tracking to bfs

--- tracking_day.py
+++ tracking_day.py
@@ -3,18 +3,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from datetime import datetime, timedelta
+from collections import deque
+from scipy import ndimage
 
 # Directory containing DAV numpy files
-dav_directory = "DAVs/"
+dav_directory = "fyp_23-trop_cyclones/DAVs"
+image_directory = "fyp_23-trop_cyclones/Images"
 
 # Set threshold values
-dav_threshold = 2850  # Adjust this threshold as needed
+dav_threshold = 2400  # Adjust this threshold as needed
 
 # Set minimum cluster size to avoid small circles
 min_cluster_size = 50  # Adjust this value as needed
 
 # Function to perform tracking
-def track_clusters(dav_array, threshold):
+def track_clusters_bfs(dav_array, fixed_threshold, min_cluster_size):
+    
+    height, width = dav_array.shape
+    visited = np.zeros((height, width), dtype=bool)
+    clusters = []
+
+    def bfs(x, y):
+        cluster = []
+        queue = deque([(x, y)])
+
+        while queue:
+            x, y = queue.popleft()
+            if visited[x, y]:
+                continue
+            visited[x, y] = True
+            cluster.append((x, y))
+
+            neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+            for dx, dy in neighbors:
+                new_x, new_y = x + dx, y + dy
+                if 0 <= new_x < height and 0 <= new_y < width and not visited[new_x, new_y] and dav_array[new_x, new_y] <= fixed_threshold:
+                    queue.append((new_x, new_y))
+
+        return cluster
+
+    for x in range(height):
+        for y in range(width):
+            if not visited[x, y] and dav_array[x, y] <= fixed_threshold:
+                new_cluster = bfs(x, y)
+                if len(new_cluster) >= min_cluster_size:
+                    clusters.append(new_cluster)
+
+    return clusters
+
+
+def track_clusters_dfs(dav_array, threshold):
     height, width = dav_array.shape
     visited = np.zeros((height, width), dtype=bool)
     clusters = []
@@ -33,14 +71,14 @@ def track_clusters(dav_array, threshold):
             neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
             for dx, dy in neighbors:
                 new_x, new_y = x + dx, y + dy
-                if 0 <= new_x < height and 0 <= new_y < width and not visited[new_x, new_y] and dav_array[new_x, new_y] >= threshold:
+                if 0 <= new_x < height and 0 <= new_y < width and not visited[new_x, new_y] and dav_array[new_x, new_y] <= threshold:
                     stack.append((new_x, new_y))
 
         return cluster
 
     for x in range(height):
         for y in range(width):
-            if not visited[x, y] and dav_array[x, y] >= threshold:
+            if not visited[x, y] and dav_array[x, y] <= threshold:  # Change the threshold condition to find below threshold
                 new_cluster = dfs(x, y)
                 if len(new_cluster) > 0:
                     clusters.append(new_cluster)
@@ -48,19 +86,24 @@ def track_clusters(dav_array, threshold):
     return clusters
 
 # Function to process and plot a single DAV image
-def process_and_plot_single_dav_array(dav_array, hour):
-    # Create a figure
+def process_and_plot_single_dav_array(dav_array, hour, image_path):
+    # Load the corresponding image
+    image = cv2.imread(image_path)
+
+    # Create subplots for Clusters and the corresponding Image
     plt.figure(figsize=(8, 6))
 
     # Define the custom colormap and normalization
-    cmap = 'jet'
-    norm = plt.Normalize(vmin=np.min(dav_array), vmax=np.max(dav_array))
+    # cmap = 'jet'
+    cmap = plt.get_cmap('jet')
+    norm = plt.Normalize(vmin=2100, vmax=3200)
+    print(np.min(dav_array), np.max(dav_array))
 
-    # Display the grayscale image with the custom colormap and normalization
+
+    # Subplot 1: Clusters
+    plt.subplot(1, 2, 1)
     plt.imshow(dav_array, cmap=cmap, norm=norm, origin='upper')
-
-    # Plot the clusters as red circles, avoiding small clusters
-    tracked_clusters = track_clusters(dav_array, dav_threshold)
+    tracked_clusters = track_clusters_bfs(dav_array, dav_threshold, min_cluster_size)
     for cluster in tracked_clusters:
         if len(cluster) >= min_cluster_size:
             x_coords, y_coords = zip(*cluster)
@@ -69,18 +112,24 @@ def process_and_plot_single_dav_array(dav_array, hour):
             cluster_radius = max(np.ptp(x_coords), np.ptp(y_coords)) / 2
             circle = plt.Circle((cluster_center_y, cluster_center_x), cluster_radius, color='red', fill=False, lw=1)
             plt.gca().add_patch(circle)
+    plt.title("Clusters")
 
-    # Add a color scale (colorbar) to the plot
-    cbar = plt.colorbar(orientation='vertical')
-    cbar.set_label('DAV Values')
+    # Subplot 2: Image
+    plt.subplot(1, 2, 2)
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))  # Display the loaded image
+    plt.title("Image")
 
     # Remove axis labels
-    plt.axis('off')
+    for ax in plt.gcf().get_axes():
+        ax.axis('off')
+
+    # Set the entire date as the figure title
+    plt.suptitle(datetime_str)
+
+    # Adjust subplot layout
     plt.tight_layout()
-    # Set the entire date as the title
-    plt.title(datetime_str)
-    # Save the plot as an image file without specifying a directory
-    #plt.savefig(f"{datetime_str}.png")
+
+    # Display the plot
     plt.show()
 
 # Define the start date and time
@@ -89,7 +138,7 @@ start_datetime = datetime(2021, 8, 11, 0, 0)
 # Define the number of hours you want to process
 num_hours = 24
 
-# Process and plot each DAV array one by one
+# Process and plot DAV arrays and images for specific hours
 for hour in range(num_hours):
     # Format the hour as a string with leading zeros
     hour_str = start_datetime.strftime("%Y%m%d%H")
@@ -97,14 +146,17 @@ for hour in range(num_hours):
     # Get the entire date as a string
     datetime_str = start_datetime.strftime("%Y-%m-%d %H:%M:%S")
     
-    # Construct the file path
+    # Construct the file path for the DAV numpy array
     file_path = os.path.join(dav_directory, f"merg_{hour_str}_DAV.npy")
     
     # Load the numpy array from the file
     dav_array = np.load(file_path)
     
-    # Process and plot the single DAV array
-    process_and_plot_single_dav_array(dav_array, hour)
+    # Construct the file path for the corresponding image
+    image_path = os.path.join(image_directory, f"merg_{hour_str}.jpg")
+    
+    # Process and plot the DAV array and image for the specific hour
+    process_and_plot_single_dav_array(dav_array, hour, image_path)
     
     # Increment the datetime by one hour
     start_datetime += timedelta(hours=1)
