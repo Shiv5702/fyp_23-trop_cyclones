6c0e4070daf761577a1fa20b4594c96ba5a3056a
Siva Thangavel <siva0004@student.monash.edu>
2023-09-19 16:25:23 +1000

    Added cluster unique IDs for tracking

--- tracking_ID.py
+++ tracking_ID.py
@@ -3,6 +3,7 @@ import numpy as np
 from datetime import datetime, timedelta
 from collections import deque
 import csv
+from scipy.spatial.distance import euclidean
 
 # Directory containing DAV numpy files
 dav_directory = "DAVs"
@@ -12,6 +13,9 @@ dav_threshold = 2400  # Adjust this threshold as needed
 
 # Set minimum cluster size to avoid small clusters
 min_cluster_size = 50  # Adjust this value as needed
+
+# Set the distance threshold for considering clusters as the same
+distance_threshold = 10  # Adjust this threshold as needed
 
 # Function to perform tracking
 def track_clusters_bfs(dav_array, fixed_threshold, min_cluster_size):
@@ -47,6 +51,17 @@ def track_clusters_bfs(dav_array, fixed_threshold, min_cluster_size):
 
     return clusters
 
+# Function to check if two cluster centers are within the distance threshold
+def are_centers_close(cluster1, cluster2):
+    center1_x = np.mean([point[1] for point in cluster1])
+    center1_y = np.mean([point[0] for point in cluster1])
+    center2_x = np.mean([point[1] for point in cluster2])
+    center2_y = np.mean([point[0] for point in cluster2])
+    
+    distance = euclidean((center1_x, center1_y), (center2_x, center2_y))
+    
+    return distance <= distance_threshold
+
 # Define the start date and time
 start_datetime = datetime(2021, 8, 1, 0, 0)  # Start from August 1, 2021, at 00:00
 
@@ -62,22 +77,25 @@ csv_filename = "all_clusters_with_datetime.csv"
 # Write the CSV header
 with open(csv_filename, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['DateTime', 'ID', 'cluster_center_x', 'cluster_center_y'])
+    csv_writer.writerow(['Date', 'Time', 'ID', 'cluster_center_x', 'cluster_center_y'])
 
 # Process DAV arrays and generate cluster data for specific hours and days
+cluster_id = 0  # Initialize cluster ID
+clusters_with_ids = []  # Store clusters with IDs
 for day in range(num_days):
     for hour in range(hours_per_day):
         # Format the hour as a string with leading zeros
         hour_str = start_datetime.strftime("%Y%m%d%H")
 
-        # Get the entire date as a string
-        datetime_str = start_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        # Get the date and time as separate strings
+        date_str = start_datetime.strftime("%Y-%m-%d")
+        time_str = start_datetime.strftime("%H:%M:%S")
 
         # Construct the file path for the DAV numpy array
         file_path = os.path.join(dav_directory, f"merg_{hour_str}_DAV.npy")
 
         if not os.path.exists(file_path):
-            print(f"File not found for date: {datetime_str}")
+            print(f"File not found for date: {date_str} {time_str}")
 
         # Load the numpy array from the file
         dav_array = np.flipud(np.load(file_path))
@@ -85,15 +103,24 @@ for day in range(num_days):
         # Process the DAV array and generate cluster data for the specific hour
         tracked_clusters = track_clusters_bfs(dav_array, dav_threshold, min_cluster_size)
         
-        # Append cluster data including date and time to the CSV file
-        with open(csv_filename, 'a', newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            for cluster_id, cluster in enumerate(tracked_clusters):
-                if len(cluster) >= min_cluster_size:
-                    x_coords, y_coords = zip(*cluster)
-                    cluster_center_x = np.mean(x_coords)
-                    cluster_center_y = np.mean(y_coords)
-                    csv_writer.writerow([datetime_str, cluster_id, cluster_center_x, cluster_center_y])
+        # Assign IDs to clusters
+        for cluster in tracked_clusters:
+            assigned_id = None
+            for i, existing_cluster in enumerate(clusters_with_ids):
+                if are_centers_close(existing_cluster, cluster):
+                    assigned_id = i
+                    break
+            if assigned_id is not None:
+                csv_row = [date_str, time_str, assigned_id, np.mean([point[1] for point in cluster]), np.mean([point[0] for point in cluster])]
+            else:
+                clusters_with_ids.append(cluster)
+                csv_row = [date_str, time_str, cluster_id, np.mean([point[1] for point in cluster]), np.mean([point[0] for point in cluster])]
+                cluster_id += 1
+            
+            # Append cluster data including date and time to the CSV file
+            with open(csv_filename, 'a', newline='') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow(csv_row)
 
         # Increment the datetime by one hour
         start_datetime += timedelta(hours=1)
